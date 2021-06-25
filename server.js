5574d853c84e96123ff16d69567ba3830840abcf
Rafael Couto <@rafael.couto@aluno.ifsp.edu.br>
2021-06-24 22:14:02 -0300

    update database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
 const url =
-'mongodb+srv://dswa5:<password>@cluster0.uudog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+'mongodb+srv://dswa5:dswa5@cluster0.uudog.mongodb.net/ifsp?retryWrites=true&w=majority';
 require('./config/database.js')(url);
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express Server escutando na porta ' + app.get('port'));
